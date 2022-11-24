2c5ab8d9fc523376263228d70fadcb256e7ada90
Wolfgang Ziegler <wziegler@live.at>
2022-11-24 22:53:53 +0100

    Add convenience methods for training/usage

--- DotNeuralNet/BackPropagationTrainingRow.cs
+++ DotNeuralNet/BackPropagationTrainingRow.cs
@@ -1,15 +1,20 @@
 ﻿namespace DotNeuralNet
 {
-  public class BackPropagationTrainingRow
-  {
-    public BackPropagationTrainingRow(double[] inputs, double[] outputs)
-    {
-      Inputs = inputs;
-      Outputs = outputs;
-    }
+	public class BackPropagationTrainingRow
+	{
+		public BackPropagationTrainingRow(double[] inputs, double[] outputs)
+		{
+			Inputs = inputs;
+			Outputs = outputs;
+		}
 
-    public double[] Inputs { get; private set; }
+		public double[] Inputs { get; private set; }
 
-    public double[] Outputs { get; private set; }
-  }
+		public double[] Outputs { get; private set; }
+
+		public static BackPropagationTrainingRow Create(Network network)
+		{
+				return new BackPropagationTrainingRow(new double[network.InputNodes.Count], new double[network.OutputNodes.Count]);
+		}
+	}
 }
--- DotNeuralNet/Network.cs
+++ DotNeuralNet/Network.cs
@@ -3,47 +3,90 @@ using System.Collections.Generic;
 
 namespace DotNeuralNet
 {
-  public class Network
-  {
-    public Network(int inputNodeCount, int hiddenNodeCount, int outputNodeCount)
-    {
-      InputNodes = Enumerable.Range(0, inputNodeCount).Select(_ => new InputNode()).ToList();
-      HiddenNodes = Enumerable.Range(0, hiddenNodeCount).Select(_ => new HiddenNode()).ToList();
-      OutputNodes = Enumerable.Range(0, outputNodeCount).Select(_ => new OutputNode()).ToList();
+	public class Network
+	{
+		public Network(int inputNodeCount, int hiddenNodeCount, int outputNodeCount)
+		{
+			InputNodes = Enumerable.Range(0, inputNodeCount).Select(_ => new InputNode()).ToList();
+			HiddenNodes = Enumerable.Range(0, hiddenNodeCount).Select(_ => new HiddenNode()).ToList();
+			OutputNodes = Enumerable.Range(0, outputNodeCount).Select(_ => new OutputNode()).ToList();
 
-      // Connect the nodes.
-      foreach (var i in InputNodes)
-      {
-        foreach (var h in HiddenNodes)
-        {
-          h.AddIncoming(i);
-        }
-      }
-      foreach (var h in HiddenNodes)
-      {
-        foreach (var o in OutputNodes)
-        {
-          o.AddIncoming(h);
-        }
-      }
-    }
+			// Connect the nodes.
+			foreach (var i in InputNodes)
+			{
+				foreach (var h in HiddenNodes)
+				{
+					h.AddIncoming(i);
+				}
+			}
+			foreach (var h in HiddenNodes)
+			{
+				foreach (var o in OutputNodes)
+				{
+					o.AddIncoming(h);
+				}
+			}
+		}
 
-    public void Invalidate()
-    {
-      foreach (var n in OutputNodes)
-      {
-        n.Invalidate();
-      }
-      foreach (var n in HiddenNodes)
-      {
-        n.Invalidate();
-      }
-    }
+		public void Invalidate()
+		{
+			foreach (var n in OutputNodes)
+			{
+				n.Invalidate();
+			}
+			foreach (var n in HiddenNodes)
+			{
+				n.Invalidate();
+			}
+		}
 
-    public IList<InputNode> InputNodes { get; private set;  }
+		public double[] GetWeights()
+		{
+			var list = new List<double>();
+			foreach (var h in HiddenNodes)
+			{
+				foreach (var i in h.Incoming)
+				{
+					list.Add(i.Weight);
+				}
+				list.Add(h.Bias);
+			}
+			foreach (var n in OutputNodes)
+			{
+				foreach (var i in n.Incoming)
+				{
+					list.Add(i.Weight);
+				}
+				list.Add(n.Bias);
+			}
+			return list.ToArray();
+		}
 
-    public IList<HiddenNode> HiddenNodes { get; private set; }
+		public void SetWeights(double[] weights)
+		{
+			var index = 0;
+			foreach (var n in HiddenNodes)
+			{
+				foreach (var i in n.Incoming)
+				{
+					i.Weight = weights[index++];
+				}
+				n.Bias = weights[index++];
+			}
+			foreach (var n in OutputNodes)
+			{
+				foreach (var i in n.Incoming)
+				{
+					i.Weight = weights[index++];
+				}
+				n.Bias = weights[index++];
+			}
+		}
 
-    public IList<OutputNode> OutputNodes { get; private set;  }
-  }
+		public IList<InputNode> InputNodes { get; private set; }
+
+		public IList<HiddenNode> HiddenNodes { get; private set; }
+
+		public IList<OutputNode> OutputNodes { get; private set; }
+	}
 }
